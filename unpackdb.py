f5bf61016f46169a795c9becd23df56f0c46372e
Mana <57663038+4d616e61@users.noreply.github.com>
2025-05-02 11:51:10 -0700

    add batch processing

--- unpackdb.py
+++ unpackdb.py
@@ -4,15 +4,22 @@ import sys
 import json
 from cfg import *
 import os
-#import yappi
+import yappi
 
-
+batch_size = 4096
 
 async def main():
     db0_n = sys.argv[1]
     db1_n = "cachedb.db" if len(sys.argv) < 3 else sys.argv[2]
+    if os.path.exists(db1_n):
+        confirm = input(f"operation will overwrite {db1_n}. confirm?(y/n) ")
+        if confirm != "y":
+            print("not confirmed. exiting...")
+            return
+
     db0 = await aiosqlite.connect(db0_n)
     db1 = await aiosqlite.connect(db1_n)
+    _ = await db1.execute("DROP TABLE IF EXISTS api_response")
     _ = await db1.execute("""CREATE TABLE api_response(
     gid INTEGER,
     resp TEXT,
@@ -25,29 +32,33 @@ async def main():
     );
     """)
     cursor_read = await db0.execute("SELECT resp from api_response");
-
+    
     while True:
-        res_row = await cursor_read.fetchone()
-        if res_row == None:
+        res_rows = await cursor_read.fetchmany(batch_size)
+        if len(res_rows) == 0:
             break
-        res_s = res_row[0]
-        res = json.loads(res_s)
-        print(res["gid"])
+        insert_vals = []
+        for res_row in res_rows:
+            res_s = res_row[0]
+            res = json.loads(res_s)
+            #print(res["gid"])
+            
+            vals = (
+                res["gid"], 
+                res_s, 
+                res["rating"], 
+                res["title"], 
+                res["title_jpn"],
+                json.dumps(res["tags"]),
+                1 if res["expunged"] else 0
+            ) 
+            insert_vals.append(vals)
         command = """
-        INSERT OR REPLACE INTO api_response 
-        (gid, resp, rating, title, title_jpn, tags_json, expunged)
-        VALUES(?, ?, ?, ?, ?, ?, ?)
-        """
-        vals = (
-            res["gid"], 
-            res_s, 
-            res["rating"], 
-            res["title"], 
-            res["title_jpn"],
-            json.dumps(res["tags"]),
-            1 if res["expunged"] else 0
-        ) 
-        cursor_write = await db1.execute(command, vals)
+            INSERT OR REPLACE INTO api_response 
+            (gid, resp, rating, title, title_jpn, tags_json, expunged)
+            VALUES(?, ?, ?, ?, ?, ?, ?)
+            """
+        cursor_write = await db1.executemany(command, insert_vals)
 
     await db1.commit()
     await db0.close()
@@ -57,6 +68,12 @@ async def main():
 
 
 if __name__ == "__main__":
+    # yappi.set_clock_type("wall")
+    # with yappi.run():
+    #     asyncio.run(main())
+    # stats = yappi.get_func_stats()
+    # stats.save("yappi.callgrind", 'callgrind')
+    # #stats.print_all()
 
     asyncio.run(main())
     
